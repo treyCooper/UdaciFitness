1c7b9589d8d3556020468774269ee975589882c1
Trey <treycoopermusic@gmail.com>
2017-12-20 19:19:26 -0500

    add live mode

--- App.js
+++ App.js
@@ -10,7 +10,7 @@ import { purple, white } from './utils/colors'
 import { FontAwesome, Ionicons } from '@expo/vector-icons'
 import { Constants } from 'expo'
 import EntryDetail from './components/EntryDetail'
-
+import Live from './components/Live'
 function UdaciStatusBar ({ backgroundColor, ...props }) {
   return (
     <View style={{backgroundColor, height: Constants.statusBarHeight}}>
@@ -33,7 +33,14 @@ const Tabs = TabNavigator({
       tabBarLabel: 'Add Entry',
       tabBarIcon: ({ tintColor }) => <FontAwesome name='plus-square' size={30} color={tintColor} />
     }
-  }
+  },
+  Live: {
+    screen: Live,
+    navigationOptions: {
+      tabBarLabel: 'Live',
+      tabBarIcon: ({ tintColor }) => <Ionicons name='ios-speedometer' size={30} color={tintColor} />
+    }
+  },
 }, {
   tabBarOptions: {
     activeTintColor: Platform.OS === 'ios' ? purple : white,
